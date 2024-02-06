771a68ca38c444a04850a3b5a8b926ba64e1666c
0xduer <0xduer@gmail.com>
2024-02-06 17:31:28 +0800

    feat:调整首页布局与样式

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,7 +1,7 @@
 import './style/index.css';
 import React from 'react';
 import {Layout, Menu} from 'antd';
-import {DashboardOutlined} from '@ant-design/icons';
+import {GithubOutlined, CopyrightOutlined, DashboardOutlined} from '@ant-design/icons';
 
 const {Header, Sider, Content} = Layout;
 
@@ -22,14 +22,20 @@ const App: React.FC = () => (
     <Layout className="container">
       <Header className="header">
         <div className="logo">AMIS Admin Pro</div>
+        <div className="navigate">
+          <div className="account"><a href="https://github.com/0xduer" target="_blank"><GithubOutlined
+            style={{fontSize: '16px', color: '#000', marginRight: '8px'}} />Lin Gui</a>
+          </div>
+        </div>
       </Header>
       <Layout className="main">
-        <Sider className="sider">
-          <Menu style={{height: '100%'}} mode="inline" items={items}
+        <Sider className="sider" theme="light">
+          <Menu mode="inline" items={items}
                 defaultOpenKeys={['dashboard']} defaultSelectedKeys={['workspace']} />
         </Sider>
         <Content className="content">
           <Workspace />
+          <div className="footer">AMIS Admin Pro <CopyrightOutlined /> Powered by Lin Gui</div>
         </Content>
       </Layout>
     </Layout>
