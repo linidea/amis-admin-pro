a35a3036fac165231e51ed601576c502ec74bb27
0xduer <0xduer@gmail.com>
2024-02-12 21:49:53 +0800

    feat:调整首页样式依赖路径

---
+++ src/style/index.ts
@@ -0,0 +1,13 @@
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/css/v4-shims.css';
+import 'amis/lib/themes/cxd.css';
+import 'amis/lib/helper.css';
+import 'amis/sdk/iconfont.css';
+import 'amis-editor-core/lib/style.css';
+import './style.scss';
+import {setDefaultTheme} from 'amis';
+import {setThemeConfig} from 'amis-editor-core';
+import themeConfig from 'amis-theme-editor-helper/lib/systemTheme/cxd';
+
+setDefaultTheme('cxd');
+setThemeConfig(themeConfig);
