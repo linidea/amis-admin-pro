45ecbd6fd1dac136247a11e2d96e36201b527588
0xduer <0xduer@gmail.com>
2024-02-12 21:50:42 +0800

    feat:新增测试页面与默认 json 配置

--- src/view/demo/Test.tsx
+++ src/view/demo/Test.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import {render as amisRender} from 'amis';
 
 export default function App() {
-  return <div>Test</div>;
+  let schema = {
+    'type': 'page',
+    'title': '测试页面',
+    'body': '这是一个测试页面'
+  };
+  let body = amisRender(schema);
+  return <div>{body}</div>;
 }
