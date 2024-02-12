a0e6349e04f3967a8d4296329901e30ad6a22239
0xduer <0xduer@gmail.com>
2024-02-13 01:58:18 +0800

    feat:新增测试页面获取本地 schema 页面配置

--- src/view/demo/Test.tsx
+++ src/view/demo/Test.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import {render as amisRender} from 'amis';
 
-export default function App() {
-  let schema = {
+
+// 设置默认的 schema
+function getSchema() {
+  let defaultSchema = {
     'type': 'page',
     'title': '测试页面',
-    'body': '这是一个测试页面'
+    'body': '这是一个测试页面,你可以在编辑器中修改这个页面的内容'
   };
+  let localSchema = localStorage.getItem('localSchema');
+  if (localSchema) {
+    return JSON.parse(localSchema);
+  } else {
+    return defaultSchema;
+  }
+}
+
+export default function App() {
+  const [schema, setSchema] = React.useState(getSchema());
+
   let body = amisRender(schema);
   return <div>{body}</div>;
 }
