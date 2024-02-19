ce6c21fbabc84f4221a8276efb0952d37c3955a5
0xduer <0xduer@gmail.com>
2024-02-19 14:31:04 +0800

    feat:优化测试无表单时引导提示

--- src/view/demo/Test.tsx
+++ src/view/demo/Test.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import {render as amisRender} from 'amis';
+import {Result, Button} from 'antd';
 
 
 // 设置默认的 schema
 function getSchema() {
-  let defaultSchema = {
-    'type': 'page',
-    'title': '测试页面',
-    'body': '这是一个测试页面,你可以在编辑器中修改这个页面的内容'
-  };
   let localSchema = localStorage.getItem('localSchema');
-  if (localSchema) {
-    return JSON.parse(localSchema);
-  } else {
-    return defaultSchema;
-  }
+  return localSchema ? JSON.parse(localSchema) : '';
 }
 
 export default function App() {
   const [schema, setSchema] = React.useState(getSchema());
 
   let body = amisRender(schema);
-  return <div>{body}</div>;
+  return <>
+    <Result
+      style={{display: getSchema() ? 'none' : 'block'}}
+      title="无可预览的页面"
+      extra={
+        <Button type="primary" target="_blank"
+                href={`${window.location.origin}${window.location.pathname}#/editor`}>
+          去设计
+        </Button>
+      }
+    />
+    <div>{body}</div>
+  </>;
 }
